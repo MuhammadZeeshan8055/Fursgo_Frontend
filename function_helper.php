dcf450278e4879c01c9cb24f62d62204dfcb47ad
Muhammad Wahab Sharif <www.rex19@gmail.com>
2026-02-26 14:41:12 +0500

    localhost updated

--- function_helper.php
+++ function_helper.php
@@ -1,6 +1,10 @@
 <?php
-if ($_SERVER['HTTP_HOST'] === 'localhost') {
-    define('BASE_URL', 'http://localhost/fursgo/');
+
+// print_r($_SERVER['HTTP_HOST']);
+// die();
+
+if ($_SERVER['HTTP_HOST'] === 'localhost:8000') {
+    define('BASE_URL', 'http://localhost:8000/');
 } else {
     define('BASE_URL', 'https://dev.zeeteck.com/projects/fursgo/');
 }
