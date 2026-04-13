4be406ecffe01240223256dc6e7e8feac48f755f
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2026-04-13 17:00:15 +0500

    link set

--- function_helper.php
+++ function_helper.php
@@ -3,8 +3,8 @@
 // print_r($_SERVER['HTTP_HOST']);
 // die();
 
-if ($_SERVER['HTTP_HOST'] === 'localhost:8001') {
-    define('BASE_URL', 'http://localhost:8001/');
+if ($_SERVER['HTTP_HOST'] === 'localhost:8000') {
+    define('BASE_URL', 'http://localhost:8000/');
 } else {
     define('BASE_URL', 'https://dev.zeeteck.com/projects/fursgo/');
 }
