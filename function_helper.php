bd50d5eeff48b36e4cba36cf766af0125df85a85
MuhammadZeeshan8055 <muhammadzeeshan8055@gmail.com>
2026-04-15 15:42:43 +0500

    Project Name

--- function_helper.php
+++ function_helper.php
@@ -6,5 +6,5 @@
 if ($_SERVER['HTTP_HOST'] === 'localhost:8000') {
     define('BASE_URL', 'http://localhost:8000/');
 } else {
-    define('BASE_URL', 'https://dev.zeeteck.com/projects/fursgo/');
+    define('BASE_URL', 'https://dev.zeeteck.com/projects/fursgo_old/');
 }
